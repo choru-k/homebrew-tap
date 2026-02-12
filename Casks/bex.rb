bde8c4a562ee987ca9afe7c5eabcc546f826193a
Choru <dev@choru.net>
2026-02-12 12:50:08 +0900

    Update Bex v0.2.0 sha256 for signed build

--- Casks/bex.rb
+++ Casks/bex.rb
@@ -1,6 +1,6 @@
 cask "bex" do
   version "0.2.0"
-  sha256 "ec14c8c7d2e4164ffced314292c7c6a4fa4da8888f6bc91d17cad3772748d02d"
+  sha256 "aa1f001ca1e08165bc74cbde1e2af794a6748e1b7f98a6c802855c3836b1045b"
 
   url "https://github.com/choru-k/bex/releases/download/v#{version}/Bex.zip"
   name "Bex"
